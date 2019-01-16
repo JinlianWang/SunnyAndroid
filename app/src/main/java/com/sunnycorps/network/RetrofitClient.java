2a56ce73c5e845c52be74909e25f2031c37c0ed6
Sunny Wang <wangjinlian@hotmail.com>
2019-01-16 07:50:10 -0500

    Add HttpLoggingInterceptor

--- app/src/main/java/com/sunnycorps/network/RetrofitClient.java
+++ app/src/main/java/com/sunnycorps/network/RetrofitClient.java
@@ -7,6 +7,8 @@ import com.sunnycorps.network.services.CustomerServices;
 
 import java.util.concurrent.CompletableFuture;
 
+import okhttp3.OkHttpClient;
+import okhttp3.logging.HttpLoggingInterceptor;
 import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Retrofit;
@@ -20,9 +22,14 @@ public class RetrofitClient {
 
     //private constructor to avoid client applications to use constructor
     private RetrofitClient(){
+        HttpLoggingInterceptor interceptor = new HttpLoggingInterceptor();
+        interceptor.setLevel(HttpLoggingInterceptor.Level.BODY);
+        OkHttpClient.Builder okHttpBuilder = new OkHttpClient.Builder().addInterceptor(interceptor);
+
         retrofit = new Retrofit.Builder()
                 .baseUrl("http://10.0.2.2:8080")
                 .addConverterFactory(GsonConverterFactory.create())
+                .client(okHttpBuilder.build())
                 .build();
     }
 
